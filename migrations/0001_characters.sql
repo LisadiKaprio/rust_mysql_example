a683ce6494dac01458b0b6dacb20e38e3412946a
LisadiKaprio <lisadikaprio@gmail.com>
2024-07-07 13:59:25 +0200

    adding characters to db, introducing character impl

--- migrations/0001_characters.sql
+++ migrations/0001_characters.sql
@@ -2,6 +2,6 @@ CREATE TABLE characters (
     name VARCHAR(50) PRIMARY KEY NOT NULL,
     birthday_season VARCHAR(10) NOT NULL,
     birthday_day INTEGER NOT NULL,
-    is_bachelor BOOLEAN NOT NULL
-    best_gift VARCHAR(50) NOT NULL,
+    is_bachelor BOOLEAN NOT NULL,
+    best_gift VARCHAR(50) NOT NULL
 )
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::mysql::*;
+use sqlx::{migrate, mysql::*, query};
 use std::env;
 use std::error::Error;
 use dotenv::dotenv;
@@ -20,6 +20,50 @@ struct Character {
     best_gift: String
 }
 
+impl Character {
+    fn new(name: String, birthday_season: Season, birthday_day: u8, is_bachelor: bool, best_gift: String) -> Character {
+        Character {
+            name, 
+            birthday_season,
+            birthday_day,
+            is_bachelor,
+            best_gift
+        }
+    }
+
+    async fn add_to_database(&self, pool: &MySqlPool) -> Result<(), Box<dyn Error>> {
+        let creation_query = "INSERT INTO characters (name, birthday_season, birthday_day, is_bachelor, best_gift) VALUES (?, ?, ?, ?, ?)";
+    
+        let query_result = query(creation_query).bind(&self.name)
+            .bind(&self.birthday_season.as_ref())
+            .bind(&self.birthday_day)
+            .bind(&self.is_bachelor)
+            .bind(&self.best_gift)
+            .execute(pool)
+            .await;
+    
+        match query_result {
+            Ok(_) => {
+                let message = format!("{} was successfully added to the database! :)", &self.name);
+                print_aesthetic_message(message);
+                Ok(())
+            },
+            Err(e) => {
+                let message = format!("An error occured when adding character {}! {}", &self.name, e.to_string());
+                print_aesthetic_message(message);
+                Ok(())
+            }
+        }    
+    }
+}
+
+fn print_aesthetic_message (message: String) {
+    println!("•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•");
+    println!(" ");
+    println!("{}", message);
+    println!(" ");
+    println!("•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•");
+}
 
 async fn connect_to_db() -> Result<MySqlPool, sqlx::Error>{
     dotenv().ok();
@@ -34,8 +78,51 @@ async fn connect_to_db() -> Result<MySqlPool, sqlx::Error>{
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>>{
     let pool = connect_to_db().await?;
+    migrate!("./migrations").run(&pool).await?;
 
     println!("Following seasons are available: {}, {}, {} and {}.", Season::Spring.as_ref(), Season::Summer.as_ref(), Season::Fall.as_ref(), Season::Winter.as_ref());
+
+    let existing_characters: Vec<Character> = vec![
+        Character {
+            name: "Abigail".to_string(),
+            birthday_season: Season::Fall,
+            birthday_day: 13,
+            is_bachelor: true,
+            best_gift: "Amethyst".to_string()
+        },
+        Character {
+            name: "Caroline".to_string(),
+            birthday_season: Season::Winter,
+            birthday_day: 7,
+            is_bachelor: false,
+            best_gift: "Fish Taco".to_string()
+        },
+        Character {
+            name: "Haley".to_string(),
+            birthday_season: Season::Spring,
+            birthday_day: 14,
+            is_bachelor: true,
+            best_gift: "Coconut".to_string()
+        },
+        Character {
+            name: "Lewis".to_string(),
+            birthday_season: Season::Spring,
+            birthday_day: 7,
+            is_bachelor: false,
+            best_gift: "Autumn's Beauty".to_string()
+        },
+        Character {
+            name: "Leah".to_string(),
+            birthday_season: Season::Winter,
+            birthday_day: 23,
+            is_bachelor: true,
+            best_gift: "Goat Cheese".to_string()
+        }
+    ];
+
+    for character in &existing_characters {
+        character.add_to_database(&pool).await?;
+    };
 
     Ok(())
 }
